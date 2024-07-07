aabe120bd60d30527b962eabaa9d8ac2c1fd62cf
Zishan Siddique <zishansiddique80@gmail.com>
2024-07-07 10:48:58 +0545

    init docker

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+
+
+import platform
+
+
+if __name__ == '__main__':
+    os_name = platform.system()
+
+
+    print(f"Hello Wordl! from {os_name} operating system on {architecture[0]} {architecture[1]} architecture")
